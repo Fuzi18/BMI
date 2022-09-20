772fbb8e2b5642069cba47e1398ab04a0644045c
Fuzi18 <Fuzi18@users.noreply.github.com>
2022-09-20 09:43:18 +0000

    deploy: f051b66fa40ff9bf64ad050cfaebfed57c843889

---
+++ main.js
@@ -0,0 +1,10 @@
+function szamitas(){
+ 
+    let mag = parseFloat(document.getElementById("magassag").value);
+    let suly = parseFloat(document.getElementById("suly").value);
+   
+    let ered = suly / (mag / 100) ** 2;
+   
+    document.getElementById("eredmeny").textContent = ered;
+   
+   }
